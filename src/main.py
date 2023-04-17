d46cd9825297b7aaca63914cf323d2856502599c
yashdeep97 <yashdeep97@gmail.com>
2023-04-16 20:42:27 -0500

    Enable DNA sequence Input

--- src/main.py
+++ src/main.py
@@ -518,6 +518,42 @@ class CLEANSubmitJobHandler(BaseHandler):
             log.warning(f'''Could not verify CAPTCHA''')
             return False
         
+    def getProteinSeqFromDNA(self, DNA_sequence):
+        # define a codon table as a dictionary
+        codon_table = {
+            'ATA':'I', 'ATC':'I', 'ATT':'I', 'ATG':'M',
+            'ACA':'T', 'ACC':'T', 'ACG':'T', 'ACT':'T',
+            'AAC':'N', 'AAT':'N', 'AAA':'K', 'AAG':'K',
+            'AGC':'S', 'AGT':'S', 'AGA':'R', 'AGG':'R',                
+            'CTA':'L', 'CTC':'L', 'CTG':'L', 'CTT':'L',
+            'CCA':'P', 'CCC':'P', 'CCG':'P', 'CCT':'P',
+            'CAC':'H', 'CAT':'H', 'CAA':'Q', 'CAG':'Q',
+            'CGA':'R', 'CGC':'R', 'CGG':'R', 'CGT':'R',
+            'GTA':'V', 'GTC':'V', 'GTG':'V', 'GTT':'V',
+            'GCA':'A', 'GCC':'A', 'GCG':'A', 'GCT':'A',
+            'GAC':'D', 'GAT':'D', 'GAA':'E', 'GAG':'E',
+            'GGA':'G', 'GGC':'G', 'GGG':'G', 'GGT':'G',
+            'TCA':'S', 'TCC':'S', 'TCG':'S', 'TCT':'S',
+            'TTC':'F', 'TTT':'F', 'TTA':'L', 'TTG':'L',
+            'TAC':'Y', 'TAT':'Y', 'TAA':'*', 'TAG':'*',
+            'TGC':'C', 'TGT':'C', 'TGA':'*', 'TGG':'W',
+        }
+
+        nucleotide_seq = DNA_sequence
+
+        # assert that the length of the nucleotide sequence is a multiple of 3
+        if len(nucleotide_seq) % 3 != 0:
+            log.error(f'''Length of nucleotide sequence is not a multiple of 3''')
+            return ''
+        # split the nucleotide sequence into codons
+        codons = [nucleotide_seq[i:i+3] for i in range(0, len(nucleotide_seq), 3)]
+        # translate each codon into its corresponding amino acid
+        amino_acids = [codon_table[codon] for codon in codons]
+        # join the resulting amino acid sequence into a string
+        amino_acid_seq = "".join(amino_acids)
+        # print the resulting amino acid sequence
+        return amino_acid_seq
+        
     def post(self):
         try:
             data = json.loads(self.request.body)
@@ -537,10 +573,16 @@ class CLEANSubmitJobHandler(BaseHandler):
             # Convert JSON to FASTA format
             for record in data['input_fasta']:
                 # Check for valid amino acid characters
-                if not re.match('^[ACDEFGHIKLMNPQRSTVWY]+$', record["sequence"]):
+                if re.match('^[ACDEFGHIKLMNPQRSTVWY]+$', record["sequence"]):
+                    job_config += ">{}\n{}\n".format(record["header"], record["sequence"])
+                elif re.match('^[ACGTURYSWKMBDHVN]*$', record["DNA_sequence"]):
+                    protein_sequence = self.getProteinSeqFromDNA(record["DNA_sequence"])
+                    if protein_sequence == '':
+                        raise Exception('Invalid DNA Sequence')
+                    else:
+                        job_config += ">{}\n{}\n".format(record["header"], protein_sequence)
+                else: 
                     raise Exception('Invalid FASTA Protein Sequence')
-                job_config += ">{}\n{}\n".format(record["header"], record["sequence"])
-
 
             ## Command that the job container will execute. The `$JOB_OUTPUT_DIR` environment variable is
             ## populated at run time after a job ID and output directory have been provisioned.
